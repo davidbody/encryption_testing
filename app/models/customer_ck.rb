2e1ddc714da07c5bf387e578135268646a862948
David W. Body <davidbody@bigcreek.com>
2015-11-04 13:18:46 -0600

    Switch to PostgreSQL PGP encryption for Crypt Keeper

--- app/models/customer_ck.rb
+++ app/models/customer_ck.rb
@@ -1,5 +1,5 @@
 class CustomerCK < ActiveRecord::Base
   self.table_name = "customers_ck"
 
-  crypt_keeper :ssn, :dob, encryptor: :aes_new, key: 'super_good_password', salt: 'salt'
+  crypt_keeper :ssn, :dob, encryptor: :postgres_pgp, key: 'super_good_password', salt: 'salt'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104190433) do
+ActiveRecord::Schema.define(version: 20151104191726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pgcrypto"
 
   create_table "customers_ae", force: :cascade do |t|
     t.string   "first_name"

37409ea9b25e5540b4a9214043aa9fb1e8b6f431
David W. Body <davidbody@bigcreek.com>
2015-11-09 11:30:55 -0600

    Updated model query benchmarks

--- performance/performance_spec.rb
+++ performance/performance_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "performance" do
+  context "unencrypted" do
+    it 'should be fast' do
+      Benchmark.bm(20) do |bm|
+        bm.report("unencrypted") do
+          10.times { Customer.where(ssn: "123-45-6789").first }
+        end
+      end
+    end
+  end
+
   context "attr_encrypted" do
     it 'should be fast' do
       Benchmark.bm(20) do |bm|
         bm.report("attr_encrypted") do
-          1000.times { CustomerAE.where(encrypted_ssn: CustomerAE.encrypt_ssn("123-45-6789")) }
+          10.times { CustomerAE.where(encrypted_ssn: CustomerAE.encrypt_ssn("123-45-6789")).first }
         end
       end
     end
@@ -15,19 +25,21 @@ RSpec.describe "performance" do
     it 'should be fast' do
       Benchmark.bm(20) do |bm|
         bm.report("crypt_keeper") do
-          1000.times { CustomerCK.search_by_plaintext(:ssn, "123-45-6789") }
+          10.times { CustomerCK.search_by_plaintext(:ssn, "123-45-6789").first }
         end
       end
     end
   end
 end
 
-# Randomized with seed 16309
+
+# Randomized with seed 31817
 #                            user     system      total        real
-# attr_encrypted         0.890000   0.000000   0.890000 (  0.890329)
+# unencrypted            0.010000   0.000000   0.010000 (  0.158169)
 #                            user     system      total        real
-# crypt_keeper           0.040000   0.010000   0.050000 (  0.045644)
+# crypt_keeper           0.010000   0.010000   0.020000 (347.166144)
+#                            user     system      total        real
+# attr_encrypted         0.010000   0.000000   0.010000 (  0.158654)
 #
-#
-# Finished in 0.93964 seconds (files took 1.48 seconds to load)
-# 2 examples, 0 failures
+# Finished in 5 minutes 47 seconds (files took 1.56 seconds to load)
+# 3 examples, 0 failures

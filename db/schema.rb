47cf6b7b9eccfd8003e7a61f843d9f761c04901c
David W. Body <davidbody@bigcreek.com>
2015-11-09 11:29:30 -0600

    Added unencrypted model for comparison

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104191726) do
+ActiveRecord::Schema.define(version: 20151109144258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "pgcrypto"
+
+  create_table "customers", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "ssn"
+    t.date     "dob"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "customers_ae", force: :cascade do |t|
     t.string   "first_name"
